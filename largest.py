3ac8f8d2c03af282eb8e1cfd7772520206f0676e
abisheek23 <abisheek23@gmail.com>
2024-06-11 11:29:54 +0530

    message

---
+++ largest.py
@@ -0,0 +1,64 @@
+# a=int(input("enter the number"))
+# b=int(input("enter the number"))
+# c=int(input("enter the number"))
+# d=int(input("enter the number"))
+# if a>b:
+#     if a>c:
+#      if a>d:
+#       print(a)
+#     else:
+#      if c>d:
+#       print(c)
+#      else:
+#        print(d)
+# else:
+#     if b>c:
+#       if b>d:
+#        print(b)
+#       else:
+#        print(d)
+#     else:
+#      if c>b:
+#       print(c)
+#      else:
+#        print(b)
+# a=int(input("enter the number"))
+# b=int(input("enter the number"))
+# c=int(input("enter the number"))
+# if a<c:
+#     if a<c:
+#         print(a,"is the smallest")
+#     else:
+#         print(c,"is the smallest")
+# else:
+#     if b<c:
+#         print(b,"is the smallest")
+#     else:
+#         print(c,"is the smallest")
+# a=int(input("enter the number"))
+# b=int(input("enter the number"))
+# c=int(input("enter the number"))
+# d=int(input("enter the number"))
+# if a>b and a>c and a>c:
+#     print(a)
+# elif b>c and b>d and b>a:
+#     print(b)
+# elif c>d and c>b and c>a:
+#     print (c)
+# else:
+#     print(d)
+# a= "welcome tho the world"
+# b=str(input("enter the word or letter"))
+# if b in a:
+#     print ("yes")
+# else:
+#     print("not ")
+# b=input("enter word")
+# if len(b)>=8 and len(b)<=12:
+#     print("valid")
+# else:
+#     print("invalid")      
+# for a in range(20):
+#     print(a)
+for a in range(15):
+    print("hallo world")
