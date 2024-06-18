5a4b678bb08635bb7f83dd94d6bb0ae5701fdda7
abisheek23 <abisheek23@gmail.com>
2024-06-18 11:32:27 +0530

    message

---
+++ demo5.py
@@ -0,0 +1,42 @@
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+"12"
+# a=int(input("enter the no"))
+# b=int(input("enter the no"))
+# while a<=b:
+#     print(a)
+#     a+=1
+
+# a=int(input("enter the no"))
+# b=int(input("enter the no"))
+# sum=0
+# while a<=b:
+#     sum+=a
+#     a+=1
+# print(sum)
+"while loop"
+# a=int(input("enter the no"))
+# b=int(input("enter the no"))
+# sum=0
+# odd=0
+# even=0
+# while a<=b:
+#     sum+=a
+#     if a%2!=0:
+#         odd+=a
+#     else:
+#         even+=a
+#     a+=1
+# print(odd,"sun of odd no")
+# print(even,"sum of natural no")
+# print(sum,"sum of even no")
+a=int(input("enter the no"))
+i=1
+fact=1
+while i<=a:
+    fact*=i
+    i+=1
+print(fact)
+
