350b3659e55b1f85234c3e2dc56b956612875b7e
abisheek23 <abisheek23@gmail.com>
2024-06-20 11:34:23 +0530

    python

---
+++ demo6.py
@@ -0,0 +1,26 @@
+"calculatur"
+# print("1.add \n 2.sub \n 3.mult \n 4.div \n 5.mod")
+# # a=int(input("enter your choice:"))
+# while True:
+#  x=int(input("enter the no:"))
+#  y=int(input("enter second no:"))
+#  a=int(input("enter your choice:"))
+#  if a==1:
+#      print(x+y)
+#  elif a==2:
+#     print(x-y)
+#  elif a==3:
+#     print(x*y)
+#  elif a==4:
+#     print(x/y)
+#  elif a==5:
+#     print(x%y)
+#  elif a>5:
+#     print("invalid no")
+#     break
+" coutinue "
+for i in range(10):
+    pass
+    # if i==5:
+    #     continue
+    # print(i)
---
+++ list.py
@@ -0,0 +1,34 @@
+# L=[1,2,'welcome',7.5,1,2]
+# print(L)
+# print(L[2])
+# if 2 in L:
+#     print()
+# else:
+#     print()
+
+# for i in L:
+#     print(i)
+# print(type(L))
+# print(len(L))
+# L[2]="helo"
+# print(L)
+# del L[1]
+# print(L)
+"add method"
+# L=[]
+# L.append(20)
+# L.append([21,22,25])
+# print(L)
+# l1=[]
+# l1.insert(1,30)
+# print(l1)
+# L.extend ([100,200,300])
+# print(L)
+"deleting method"
+l=[10,20,30,40,60,50]
+l.remove(10)
+print(l)
+l.pop()
+print(l)
+l.clear()
+print(l)
