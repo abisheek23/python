5da718aee59ad0e477b5e1b5114748c1dac5596a
abisheek23 <abisheek23@gmail.com>
2024-06-22 11:20:40 +0530

    python

--- list.py
+++ list.py
@@ -28,10 +28,39 @@
 
 
 "deleting method"
-l=[10,20,30,40,60,50]
-l.remove(10)
-print(l)
-l.pop()
-print(l)
-l.clear()
-print(l)
+# l=[10,20,30,40,60,50]
+# l.remove(10)
+# print(l)
+# l.pop()
+# print(l)
+# l.clear()
+# print(l)
+"to remove the similer value in a list"
+# a=[]
+# l=[10,20,30,40,40,50,]
+# for i in l:
+#     if i not in a:
+#      a.append(i)
+# print(a)
+"print a list of string and int and find the sum of intiger"
+
+# l=[12,13,'a','b','c']
+# sum=0
+# for i in l:
+#     if type (i)==int or type (i)==float:
+#      sum+=i
+# print(sum)
+
+"print a list and find the largest no"
+"1"
+# l=[10,20,40,30,]
+# l.sort()
+# print(l[-1])
+"2"
+# l=[10,20,40,30,]
+# print(max(l))
+
+a=[](input("enter the list"))
+print (a)
+print(max(a))
+
