a5eaa3dd1b96d6c0593fa0c93a0dc05f9f10649f
abisheek23 <abisheek23@gmail.com>
2024-07-01 11:32:38 +0530

    dict

--- dictionary_loop.py
+++ dictionary_loop.py
@@ -1,12 +1,27 @@
-std=[]
-for i in range(3):
-    name=input('enter the name:')
-    age=int(input('enter the age:'))
-    place=input('enter the place:')
-    ph_no=int(input('enter the phone no:'))
-    std.append({'name':name,'age':age,'place':place,'ph_no':ph_no,})
-print(std)
-print("{:<10}{:<6}".format('name','age'))
-print('_'*20)
-for i in std:
-    print("{:<10}{:<6}")
+"example"
+# std=[]
+# for i in range(2):
+#     name=input('enter the name:')
+#     age=int(input('enter the age:'))
+#     place=input('enter the place:')
+#     # ph_no=int(input('enter the phone no:'))
+#     std.append({'name':name,'age':age,'place':place,})
+# print(std)
+# print("{:<10}{:<10}{:<10}".format('name','age','plase',))
+# print('_'*25)
+# for i in std:
+#     print("{:<10}{:<10}{:<10}".format (i["name"],i["age"],i["place"]))
+# print('age>30')
+# print("{:<10}{:<10}{:<10}".format('name','age','plase',))
+# for i in std:
+#    if i['age']>=30:
+#         print("{:<10}{:<10}{:<10}".format (i["name"],i["age"],i["place"]))
+# print('age<30')
+# print("{:<10}{:<10}{:<10}".format('name','age','plase',))
+# for i in std:
+#      if i['age']<30:
+#         print("{:<10}{:<10}{:<10}".format (i["name"],i["age"],i["place"]))
+number={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+a=[(input('number'))]
+print (a)
+
