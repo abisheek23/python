cded0634f82aa9c0aee3fbf67c3db1cd01829f50
abisheek23 <abisheek23@gmail.com>
2024-07-02 11:26:33 +0530

    function

--- dictionary_loop.py
+++ dictionary_loop.py
@@ -21,7 +21,24 @@
 # for i in std:
 #      if i['age']<30:
 #         print("{:<10}{:<10}{:<10}".format (i["name"],i["age"],i["place"]))
-number={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
-a=[(input('number'))]
-print (a)
+# number={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+# a=[(input('number'))]
+# a=str(a)
+# result=""
+# for char in a:
+#     digit=int(char)
+#     if digit in number:
+#         result +=number[digit]+""
+# print(result.strip())
+
+number = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+a = int(input('Enter a number: '))
+result = ''
+while a > 0:
+    digit = a % 10  
+    word = number[digit]  
+    result = word + ' ' + result 
+    a = a // 10 
+result = result
+print(result)
 
---
+++ function.py
@@ -0,0 +1,5 @@
+def fun_1():
+    a=int(input("enter the no"))
+    b=int(input("enter the no"))
+    print(a+b)
+fun_1()
