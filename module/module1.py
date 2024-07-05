876c59d6de3a1a8c521b50e568ca133e47a0a710
abisheek23 <abisheek23@gmail.com>
2024-07-05 10:54:10 +0530

    message

---
+++ module/module1.py
@@ -0,0 +1,18 @@
+# def fun():
+#     print("welcome")
+
+# def fun1():
+#     print("halo world")
+# def fun2():
+#     print(",mnbvcxzsadfghjkioiuytrew234567890")
+# def fun(a,b):
+#     print(a+b)
+#     print(a-b)
+#     print(a%b)
+#     print(a*b)
+#     print(a**b)
+# def add(name,age=20):
+#     return name,age
+def add(name,age):
+    print('name :',name)
+    print('age',age)
---
+++ module/module2.py
@@ -0,0 +1,11 @@
+# import module1
+# module1.fun2()
+# import module1 as m1
+# m1.fun()
+# from module1 import fun,fun1
+# fun()
+# fun1()
+from module1 import*
+add('ab',20)
+add('avg',25)
+add('awe',10)
