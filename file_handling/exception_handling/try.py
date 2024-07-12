6b7211eacc5c9c794fb05accb1d7a88857dd5b4c
abisheek23 <abisheek23@gmail.com>
2024-07-12 11:23:26 +0530

    sample

---
+++ file_handling/exception_handling/try.py
@@ -0,0 +1,26 @@
+# while True:
+#     try:
+#         a=int(input("enter  the number:"))
+#         print(a)
+#         break
+#     except:
+#         print("enter a number")
+
+# l=[1,2,3,4.5,'abc',10]
+# sum=0
+# for i in l:
+#         try:
+#             sum+=i         
+#     # if type (i)==int or type (i)==float:
+#         except:
+#             pass
+# print(sum)
+
+try:
+    print(10+'35')
+except TypeError:
+    print('its type error')
+else:
+    print("else")
+finally:
+    print('finally')
