449844fe5cf5ff15c3cf5a2815add936ca26ff2c
abisheek23 <abisheek2397@gmail.com>
2024-07-18 11:29:41 +0530

    polimorfism

--- polimorphism.py
+++ polimorphism.py
@@ -1,14 +1,15 @@
 class sample:
-    def display(self):
+    def __init__(self,name,age):
         print("sample class display method")
+        print(name,age)
     def s1(self):
         print("sample class s1 method")
 
 class demo(sample):
-    def display(self):
-        super().display()
+    def __init__(self,name,age):
+        print(name,age)
+        super().__init__(name,age)
         print("demo class display method")
     def d1(self):
         print("demo class d1 method")
-obj=demo()
-obj.display()
+obj=demo("akhil",34)
